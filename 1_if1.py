4cefe1adff8588f541ab3873e1e16ddcf87c6933
Vladimir P. <oklok111@yandex.ru>
2026-03-06 00:39:34 +0300

    add branch home work for 1_if1 and done it

--- 1_if1.py
+++ 1_if1.py
@@ -15,10 +15,23 @@
 """
   
 def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+  user_year = int(input('укажите ваш возраст: '))
+
+  def check_year(user_year):
+    if user_year == 0:
+      return f'Ваш возраст: {user_year}, пора спать.'
+    elif user_year > 0 and user_year <= 6:
+      return f'Ваш возраст: {user_year}, пора в садик.'
+    elif user_year > 6 and user_year <= 17:
+      return f'Ваш возраст: {user_year}, пора в школу.'
+    elif user_year > 18 and user_year <= 21:
+      return f'Ваш возраст: {user_year}, пора в ВУЗ.'
+    elif user_year > 22 and user_year <= 69:
+      return f'Ваш возраст: {user_year}, пора на работу.'
+    else:
+      return 'Пора на бесконечную пенсию'
+
+  user_can_go = check_year(user_year)
+  print(user_can_go)
 if __name__ == "__main__":
     main()
